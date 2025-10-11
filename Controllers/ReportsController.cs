12260d762e51bf43ecc769691c855a75b719fbde
Marciopssss <marceldahdah278@icloud.com>
2025-10-11 19:06:41 +0300

    Added lists of expired users

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Linq;
-using System.Web.Mvc;
 using Gym_Membership.Data;
 using Microsoft.AspNetCore.Mvc;
 using Gym_Membership.Models; // change to your actual namespace
 
-namespace YourProjectName.Controllers
+namespace Gym_Membership.Controllers
 {
     public class ReportsController : Controller
     {
@@ -16,14 +15,14 @@ namespace YourProjectName.Controllers
         {
             var expiredMembers = from m in db.Customers
                                  join p in db.Pays on m.CustomerID equals p.CustomerID
-                                 join plan in db.Memberships on p.MembershipID equals plan.PlanID
-                                 where p.ExpiryDate < DateTime.Now
+                                 join plan in db.Memberships on p.PaysID equals plan.MembershipID
+                                 where p.PaymentDate < DateTime.Now
                                  select new ExpiredMemberViewModel
                                  {
-                                     MemberID = m.MemberID,
+                                     MemberID = m.CustomerID,
                                      Name = m.Name,
-                                     PlanName = plan.PlanName,
-                                     ExpiryDate = p.ExpiryDate
+                                     PlanName = plan.Type,
+                                     ExpiryDate = p.PaymentDate
                                  };
 
             return View(expiredMembers.ToList());
